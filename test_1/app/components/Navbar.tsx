28194d34df65320c6c6f9ba0ca1d3a9a2253bfe9
Asel <asel.alimbaeva.kk@gmail.com>
2023-05-31 08:09:16 +0300

    docs: delete folder

---
+++ test_1/app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Link from 'next/link';
+import Search from './Search';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl">
+        <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
+            <Link href="/">WikiRoket!</Link>
+            <Link href="/about">Go to About page</Link>
+            <Link href="/users">Users</Link>
+        </h1>
+        {/* <Search /> */}
+    </nav>
+  )
+}
